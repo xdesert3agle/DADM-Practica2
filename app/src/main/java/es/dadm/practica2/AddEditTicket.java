9f665a76823c50cbd4ec125c98ab0b63ac9e7fa6
Miguel Ángel <miguel.vicente@goumh.umh.es>
2018-05-22 00:43:50 +0200

    Añadida petición de servicios para la cámara y para la galería en Añadir/Editar ticket

--- app/src/main/java/es/dadm/practica2/AddEditTicket.java
+++ app/src/main/java/es/dadm/practica2/AddEditTicket.java
@@ -1,13 +1,17 @@
 package es.dadm.practica2;
 
+import android.Manifest;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Color;
+import android.location.LocationManager;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.text.InputFilter;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -26,8 +30,11 @@ import butterknife.BindView;
 import butterknife.ButterKnife;
 import pl.aprilapps.easyphotopicker.DefaultCallback;
 import pl.aprilapps.easyphotopicker.EasyImage;
+import pub.devrel.easypermissions.AfterPermissionGranted;
+import pub.devrel.easypermissions.AppSettingsDialog;
+import pub.devrel.easypermissions.EasyPermissions;
 
-public class AddEditTicket extends AppCompatActivity implements View.OnClickListener{
+public class AddEditTicket extends AppCompatActivity implements View.OnClickListener, EasyPermissions.PermissionCallbacks{
     @BindView(R.id.ivImg) ImageView ivTicketImg;
     @BindView(R.id.fabActionMenu) FloatingActionsMenu fabActionMenu;
     @BindView(R.id.fabPhotoFromGallery) FloatingActionButton fabPhotoFromGallery;
@@ -39,7 +46,14 @@ public class AddEditTicket extends AppCompatActivity implements View.OnClickList
     @BindView(R.id.btnCreateEditTicket) Button btnCreateEditTicket;
     @BindView(R.id.toolbar) Toolbar mToolbar;
 
-    private TicketDB mTicketDB;
+    public final static int CAMERA_REQUEST = 1;
+    public final static int GALLERY_REQUEST = 2;
+    public final static int EXTERNAL_REQUEST = 3;
+
+    LocationManager locationManager;
+    String provider;
+
+    private TicketDB mTicketDB = TicketDB.getInstance();
     private String mImgName;
     private Ticket mNewTicket;
     private Ticket mSelTicket;
@@ -57,13 +71,11 @@ public class AddEditTicket extends AppCompatActivity implements View.OnClickList
 
         etPrice.setFilters(new InputFilter[] {new DecimalDigitsInputFilter(2)});
 
-        // Se colocan los iconos a los fab
+        // Se colocan los iconos a los botones de la galería y de la cámara
         fabPhotoFromGallery.setIconDrawable(ImgUtil.getFontAwesomeIcon(FontAwesome.Icon.faw_image, Color.WHITE, 26, AddEditTicket.this));
         fabPhotoFromCamera.setIconDrawable(ImgUtil.getFontAwesomeIcon(FontAwesome.Icon.faw_camera, Color.WHITE, 26, AddEditTicket.this));
 
-        mTicketDB = TicketDB.getInstance();
-
-        if (isEditMode()){ // Si el usuario ha entrado a editar
+        if (isEditMode()){ // Si el usuario ha entrado a editar un ticket...
             int targetTicketID = getIntent().getExtras().getInt(fragmentList.TAG_TICKET_POSITION);
 
             mSelTicket = mTicketDB.getTicketWithID(targetTicketID);
@@ -75,7 +87,7 @@ public class AddEditTicket extends AppCompatActivity implements View.OnClickList
             btnCreateEditTicket.setText(R.string.BTN_EDIT_TICKET);
 
             mToolbar.setTitle(R.string.TITLE_EDIT_TICKET);
-        } else { // Si el usuario ha entrado a añadir un ticket nuevo
+        } else { // Si el usuario ha entrado a añadir un ticket nuevo...
             mToolbar.setTitle(R.string.TITLE_ADD_TICKET);
         }
 
@@ -103,17 +115,17 @@ public class AddEditTicket extends AppCompatActivity implements View.OnClickList
                 break;
 
             case R.id.fabPhotoFromGallery: // Opción 1 presionada. La foto viene de la galería
-                EasyImage.openGallery(AddEditTicket.this, 0);
-                fabActionMenu.collapse();
+                openGallery();
 
                 break;
 
             case R.id.fabPhotoFromCamera: // Opción 2 presionada. La foto viene de la cámara de fotos
-                EasyImage.openCamera(AddEditTicket.this, 0);
-                fabActionMenu.collapse();
+                openCamera();
 
                 break;
         }
+
+        fabActionMenu.collapse();
     }
 
     @Override
@@ -140,6 +152,53 @@ public class AddEditTicket extends AppCompatActivity implements View.OnClickList
                 ivTicketImg.setImageBitmap(bmTicketImg);
             }
         });
+    }
+
+    private void openCamera() {
+        String[] perms = {Manifest.permission.CAMERA, Manifest.permission.READ_EXTERNAL_STORAGE};
+
+        if (EasyPermissions.hasPermissions(this, perms)) {
+            EasyImage.openCamera(AddEditTicket.this, 0);
+        } else {
+            EasyPermissions.requestPermissions(this, getString(R.string.MSG_CAMERA_RAT),
+                    CAMERA_REQUEST, perms);
+        }
+    }
+
+    private void openGallery() {
+        String[] perms = {Manifest.permission.READ_EXTERNAL_STORAGE};
+
+        if (EasyPermissions.hasPermissions(this, perms)) {
+            EasyImage.openGallery(AddEditTicket.this, 0);
+        } else {
+            EasyPermissions.requestPermissions(this, getString(R.string.MSG_GALLERY_RAT),
+                    GALLERY_REQUEST, perms);
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        EasyPermissions.onRequestPermissionsResult(requestCode, permissions, grantResults, this);
+    }
+
+    @Override
+    public void onPermissionsGranted(int requestCode, @NonNull List<String> perms) {
+        switch (requestCode) {
+            case CAMERA_REQUEST:
+                EasyImage.openCamera(AddEditTicket.this, 0);
+                break;
+            case GALLERY_REQUEST:
+                EasyImage.openGallery(AddEditTicket.this, 0);
+        }
+    }
+
+    @Override
+    public void onPermissionsDenied(int requestCode, @NonNull List<String> perms) {
+        if (EasyPermissions.somePermissionPermanentlyDenied(this, perms)) {
+            new AppSettingsDialog.Builder(this).build().show();
+        }
     }
 
     public void fetchNewTicketInfo(){
